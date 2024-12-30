bc3028101924ee9289345fb84d67144daa737bab
xuwei11 <xuwei11@kuaishou.com>
2024-12-31 01:02:06 +0800

    feature: add init

---
+++ src/i18n.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import enTranslation from './locales/en.json';
+import zhTranslation from './locales/zh.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation
+      },
+      zh: {
+        translation: zhTranslation
+      }
+    },
+    fallbackLng: 'zh',
+    debug: process.env.NODE_ENV === 'development',
+    interpolation: {
+      escapeValue: false
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage']
+    }
+  });
+
+export default i18n; 
